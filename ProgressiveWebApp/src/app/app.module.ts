9ba86c2ad28f10bbbc1b122714def2867209a957
Rodrigo <rodrigooliveiravarela@gmail.com>
2018-10-01 00:08:48 -0300

    Adicionado provider contact para gerenciamento do CRUD e renomeado página contact para contactList

--- ProgressiveWebApp/src/app/app.module.ts
+++ ProgressiveWebApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ContactProvider } from '../providers/contact/contact';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContactProvider
   ]
 })
 export class AppModule {}
