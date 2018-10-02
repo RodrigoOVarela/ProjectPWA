f225a27c5c94ad3324848229d0203eab107084fc
Rodrigo <rodrigooliveiravarela@gmail.com>
2018-10-02 00:06:20 -0300

    Feito CRUD para manipulação dos dados

--- ProgressiveWebApp/src/providers/contact/contact.ts
+++ ProgressiveWebApp/src/providers/contact/contact.ts
@@ -1,17 +1,54 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-/*
-  Generated class for the ContactProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+import { Storage } from '@ionic/storage';
+import { DatePipe } from '@angular/common';
 @Injectable()
 export class ContactProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ContactProvider Provider');
+  constructor(private storage: Storage, private datepipe: DatePipe ) {}
+
+  public insert(contact: Contact){
+    let key = this.datepipe.transform(new Date(), "ddMMyyyyHHmmss");
+    return this.save(key, contact);
   }
 
+  public update(key: string, contact: Contact){
+    return this.save(key, contact);
+  }
+
+  private save(key: string, contact: Contact){
+    return this.storage.set(key,contact);
+  }
+
+  public remove(key: string){
+    return this.storage.remove(key)
+  }
+
+  public getAll() {
+    let contacts: Contactlist[] = [];
+    this.storage.forEach((value: Contact, key: string, itrationNumber: Number) => {
+      let contact = new Contactlist();
+      contact.key = key;
+      contact.contact = value;
+      contacts.push(contact);
+    })
+      .then(() => {
+        return Promise.resolve(contacts);
+      })
+      .catch((error) => {
+        return Promise.reject(error);
+      });
+  }
+
+}
+
+export class Contact{
+  name: string;
+  phone: number;
+  birth: Date;
+  active: boolean;
+}
+
+export class Contactlist{
+ key: string;
+ contact: Contact;
 }
