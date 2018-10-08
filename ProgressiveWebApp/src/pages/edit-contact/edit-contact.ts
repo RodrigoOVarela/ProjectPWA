63772b3c2dad2815125cc2d05cd23a086f5d40f4
Rodrigo <rodrigooliveiravarela@gmail.com>
2018-10-07 21:01:30 -0300

    Criado métodos da edit controller para salvar e atualizar contatos

--- ProgressiveWebApp/src/pages/edit-contact/edit-contact.ts
+++ ProgressiveWebApp/src/pages/edit-contact/edit-contact.ts
@@ -1,19 +1,44 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { ContactProvider, Contact } from '../../providers/contact/contact';
 
 
 @IonicPage()
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html',
+  selector: 'page-edit-contact',
+  templateUrl: 'edit-contact.html',
 })
 export class EditContactPage {
+  model: Contact;
+  key: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private contactProvider: ContactProvider, private toast: ToastController ) {
+ 
+    if(this.navParams.data.contact && this.navParams.data.key) {
+      this.model = this.navParams.data.contact;
+      this.key = this.navParams.data.key;
+    } else {
+      this.model = new Contact();
+    }
+ }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditContactPage');
-  }
+ save(){
+    this.saveContact()
+      .then(() => {
+        this.toast.create({ message: 'Contato Salvo.', duration: 3000, position: 'botton' }).present();
+        this.navCtrl.pop();
+      })
+      .catch(() => {
+        this.toast.create({ message: 'Erro ao Salvar o Contato.', duration: 3000, position: 'botton' }).present();
+      })
+ }
+
+ private saveContact(){
+    if(this.key){
+      return this.contactProvider.update(this.key, this.model);
+    } else {
+      return this.contactProvider.insert(this.model);
+    }
+ }
 
 }
